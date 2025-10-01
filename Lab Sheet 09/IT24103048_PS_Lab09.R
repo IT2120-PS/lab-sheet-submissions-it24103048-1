04adcd72da1fdf589540f5a9de7648e9af13e432
Subasingha SAPR <it24103048@my.sliit.lk>
2025-10-01 21:40:09 +0530

    lab 09

---
+++ Lab Sheet 09/IT24103048_PS_Lab09.R
@@ -0,0 +1,9 @@
+
+setwd("C://Users//VICTUS//OneDrive//Desktop//IT24103048")
+
+
+#1
+times <- c(44, 47, 45, 46, 43, 45, 44, 48, 46, 47, 45, 46, 44, 45, 47, 46, 45, 44, 46, 47, 45, 46, 44, 45, 46)
+
+#2
+t.test(times, mu = 46, alternative = "less")
